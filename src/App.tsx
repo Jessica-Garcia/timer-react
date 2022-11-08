2ddf3b0cd11635c9a9c74a836f88da269103ea26
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-11-08 16:12:33 -0300

    refactor: render pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-export function App() {
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
 
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
