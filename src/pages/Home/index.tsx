695d69dfd5c3b08c199a1e5ce34e09c9ee2435f2
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-01-11 15:40:10 -0300

    refactor: improve home page inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,12 +18,24 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+          </datalist>
+
           <label htmlFor="minutesAmout">durante:</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            max={60}
+            min={5}
           />
           <span>minutos.</span>
         </FormContainer>
